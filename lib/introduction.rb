024e2a53cdf03a1f47ca94049854d4549f726663
cjunks94 <cjunks94@gmail.com>
2018-03-19 17:14:36 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,10 @@
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
+<<<<<<< HEAD
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
  
+=======
+>>>>>>> 42d307627ee357d5b20fdaeeb703ca0e8e883787
